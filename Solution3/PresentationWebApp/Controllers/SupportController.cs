46bf462f6a347ef933527496f9d6ea114948672c
ryan.attard <ryan.attard@mcast.edu.mt>
2020-10-26 11:20:43 +0100

    Lesson 3 - Setting up the basis for our clean architecture

--- Solution3/PresentationWebApp/Controllers/SupportController.cs
+++ Solution3/PresentationWebApp/Controllers/SupportController.cs
@@ -20,7 +20,17 @@ namespace PresentationWebApp.Controllers
 
             //...inform the responsible staff
 
-            ViewData["feedback"] = "Thank you for getting in touch with us. We will answer back asap";
+            /*
+             * feedback = [Thank you for getting in touch with us. We will answer back asap]
+             * warning = [Type in some question]
+             * key = [value]
+             * key = [value]
+             */
+
+            if (string.IsNullOrEmpty(query))
+                ViewData["warning"] = "Type in some question";
+            else
+                ViewData["feedback"] = "Thank you for getting in touch with us. We will answer back asap";
 
             return View();
         }
